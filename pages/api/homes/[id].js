72fa4f31e7aa4e30c918ba92bab143ec9993b32a
johnny2smooth <johnbp13@gmail.com>
2022-07-28 11:21:50 -0700

    able to delete posts. edit and delete buttons added when session sees that we are the owner of the home.

--- pages/api/homes/[id].js
+++ pages/api/homes/[id].js
@@ -1,5 +1,11 @@
 import { getSession } from 'next-auth/react';
 import { PrismaClient } from '@prisma/client';
+import { createClient } from '@supabase/supabase-js';
+
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_KEY
+);
 
 const prisma = new PrismaClient();
 
@@ -29,6 +35,23 @@ export default async function handler(req, res) {
         where: { id },
         data: req.body,
       });
+      // remove image from supabase
+      if (home.image) {
+        const path = home.image.split(`${process.env.SUPABASE_BUCKET}/`)?.[1];
+        await supabase.storage.from(process.env.SUPABASE_BUCKET).remove([path]);
+      }
+      res.status(200).json(home);
+    } catch (e) {
+      res.status(500).json({ message: 'Something went wrong' });
+    }
+  }
+
+  // Delete home
+  else if (req.method === 'DELETE') {
+    try {
+      const home = await prisma.home.delete({
+        where: { id },
+      });
       res.status(200).json(home);
     } catch (e) {
       res.status(500).json({ message: 'Something went wrong' });
@@ -37,7 +60,7 @@ export default async function handler(req, res) {
 
   // HTTP method not supported!
   else {
-    res.setHeader('Allow', ['PATCH']);
+    res.setHeader('Allow', ['PATCH', 'DELETE']);
     res
       .status(405)
       .json({ message: `HTTP method ${req.method} is not supported.` });
